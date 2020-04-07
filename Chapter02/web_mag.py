4774dc0f60c8595d0687990709050c2d01bd5ced
PhlipTung <philip.dongfei@gmail.com>
2020-04-07 11:34:47 +0800

    modify write demo scrape magnet,ed2k

--- Chapter02/web_mag.py
+++ Chapter02/web_mag.py
@@ -9,13 +9,11 @@ if __name__ == "__main__":
     #print(html)
     #matches = re.findall(r"\圓\d+", html)
     #print(matches)
-    '''
 
     matches = re.findall(r'data-magnet="(magnet:?[^\"]+)', html)
     for m in matches:
         print(m)
         print()
-    '''
     matches = re.findall(r'data-ed2k="(ed2k:?[^\"]+)', html)
     for m in matches:
         print(m)
